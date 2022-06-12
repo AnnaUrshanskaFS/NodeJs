e97c40708edfe873943287f415a4d17c9950bb89
AnnaFullStack <ursanskayanna@gmail.com>
2022-06-12 11:33:34 +0300

    chalk added

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const morgan = require("morgan");
 const app = express();
 const port = 3000;
 const users = require("./routes/users");
-
+const chalk = require("chalk");
+const log = console.log;
+const error = chalk.bold.red;
+const success = chalk.bold.yellow;
 app.use(morgan(":method :url :status :response-time ms"));
 
 app.get("/", (req, res) => {
@@ -15,11 +18,11 @@ app.use(express.static("public"));
 app.use("/api/users", users);
 mongoose
   .connect("mongodb://localhost:27017/rest_api")
-  .then(() => console.log("Connected to MongoDB..."))
+  .then(() => log(success("Connected to MongoDB...")))
   .then(() => {
     app.listen(port, () => {
-      console.info(`start server start listening on port ${port}`);
+      console.info(success(`start server start listening on port ${port}`));
     });
   })
-  .catch((err) => console.error(err));
+  .catch((err) => console.error(error(err)));
 app.use(express.json());
